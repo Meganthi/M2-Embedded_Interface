1527e9cd5839f76eefda1e1c3f33aa44e4a81832
Meganthi <94211854+Meganthi@users.noreply.github.com>
2021-12-01 20:42:35 +0530

    Update solartracker.c

--- 3_Implementation/src/solartracker.c
+++ 3_Implementation/src/solartracker.c
@@ -1,1 +1,25 @@
 #include <Servo.h>
+void loop() 
+{
+  val1 = analogRead(ldr1);            
+  val2 = analogRead(ldr2);
+  val1 = map(val1, 0, 1023, 0, 180);     
+  val2 = map(val2, 0, 1023, 0, 180);     
+  if(val1 > (val2+50))
+  {    
+    if(pos<180)
+     pos=pos+1;
+    myservo.write(pos);
+    Serial.println("backward");         
+    delay(10);         
+  }
+  else if(val2 > (val1+50))
+  {
+    if(pos>0)
+     pos=pos-1;
+    myservo.write(pos);
+    Serial.println("forward");         
+    delay(10); 
+  }
+                     
+}
