1c284398fbdd77beeffc42bd46324eb3438d65ec
Meganthi <94211854+Meganthi@users.noreply.github.com>
2021-12-01 19:59:35 +0530

    Update solartracker.h

--- 3_Implementation/inc/solartracker.h
+++ 3_Implementation/inc/solartracker.h
@@ -1,1 +1,15 @@
+/**
+@file solartracker.h
+@author meganthi
+@brief Functions to handle solar tracker
+@version 0.1
+@date 2021-12-01
+@copyright Copyright (c) 2021
+*/
 
+#ifndef __TRACKING_H 
+#define __TRACKING_H
+
+#include <Servo.h>
+
+#endif
